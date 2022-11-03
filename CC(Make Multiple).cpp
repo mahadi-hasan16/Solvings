375dc2f6edac112cd33ba49bb619d90b990496b9
Md. Mahadi Hasan <mahadi.hasan.khan16@gmail.com>
2022-11-04 00:58:29 +0600

    Create CC(Make Multiple).cpp

---
+++ CC(Make Multiple).cpp
@@ -0,0 +1,43 @@
+/*
+MD. MAHADI HASAN
+Dept. of Computer Science and Telecommunication Engineering (CSTE).
+Noakhali Science and Technology University (NSTU).
+*/
+#include<bits/stdc++.h>
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define pi acos(-1)
+#define lcm(a,b) (a*b)/__gcd(a,b)
+#define all(v) v.begin(),v.end()
+#define w(x) int x;scanf("%d",&x);while(x--)
+#define ios ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define nl "\n"
+#define Case(cs) printf("Case %d: ",++cs)
+#define debug(x) cout<<x<<" ";
+typedef long long ll;
+using namespace std;
+class Maha{
+public:
+    Maha()
+    {
+        w(t)
+        {
+            int a,b;
+            cin>>a>>b;
+            if(b>=2*a or b%a==0)
+                cout<<"YES\n";
+            else
+                cout<<"NO\n";
+        }
+    }
+};
+
+signed main()
+{
+    //ios::sync_with_stdio(0);
+   //cin.tie(0);
+   //cout.tie(0);
+    Maha maha;
+    return 0;
+}
