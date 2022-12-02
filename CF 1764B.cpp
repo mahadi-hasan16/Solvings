b444c26d5939b0ca4765aeb9d53bb5fa65182968
Md. Mahadi Hasan <mahadi.hasan.khan16@gmail.com>
2022-12-02 23:47:01 +0600

    Create CF 1764B.cpp

---
+++ CF 1764B.cpp
@@ -0,0 +1,45 @@
+/*
+MD. MAHADI HASAN
+Dept. of Computer Science and Telecommunication Engineering (CSTE).
+Noakhali Science and Technology University (NSTU).
+*/
+#include<bits/stdc++.h>
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define pi acos(-1)
+#define lcm(a,b) (a*b)/__gcd(a,b)
+#define all(v) v.begin(),v.end()
+#define w(x) int x;scanf("%d",&x);while(x--)
+#define ios ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define nl "\n"
+#define Case(cs) printf("Case %d: ",++cs)
+#define debug(x) cout<<x<<" ";
+typedef long long ll;
+using namespace std;
+class Maha{
+public:
+    Maha()
+    {
+        w(t)
+        {
+            int n,x,gcd=0,i;
+            cin>>n;
+            for(i=0;i<n;i++)
+            {
+                cin>>x;
+                gcd=__gcd(gcd,x);
+            }
+            cout<<x/gcd<<nl;
+        }
+    }
+};
+
+signed main()
+{
+    //ios::sync_with_stdio(0);
+   //cin.tie(0);
+   //cout.tie(0);
+    Maha maha;
+    return 0;
+}
