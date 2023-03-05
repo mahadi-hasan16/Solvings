4fa184dbbd085090dc25cdd8e6a1a8d9a7486de8
Md. Mahadi Hasan <mahadi.hasan.khan16@gmail.com>
2023-03-06 02:16:37 +0600

    Create A - Packing Rectangles.cpp

---
+++ ITMO Academy: Pilot Course/Binary Search/Step 2/A - Packing Rectangles.cpp
@@ -0,0 +1,52 @@
+/*
+MD. MAHADI HASAN
+Dept. of Computer Science and Telecommunication Engineering (CSTE).
+Noakhali Science and Technology University (NSTU).
+*/
+#include<bits/stdc++.h>
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define pi acos(-1)
+#define lcm(a,b) (a*b)/__gcd(a,b)
+#define all(v) v.begin(),v.end()
+#define w(x) int x;scanf("%d",&x);while(x--)
+#define ios ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define nl "\n"
+#define Case(cs) printf("Case %d: ",++cs)
+#define debug(x) cout<<x<<" ";
+typedef long long ll;
+using namespace std;
+ll mod=10e9+7;
+class Maha{
+public:
+    Maha()
+    {
+        ll w,h,n,mid,x;
+        cin>>w>>h>>n;
+        ll l=1,r;
+        x=1;
+        while((x/h)*(x/w)<n)
+            x*=2;
+        r=x;
+        while(r-l>1)
+        {
+            mid=l+(r-l)/2;
+            x=(mid/h)*(mid/w);
+            if(x>=n)
+                r=mid;
+            else
+                l=mid;
+        }
+        cout<<r<<nl;
+    }
+};
+ 
+signed main()
+{
+    //ios::sync_with_stdio(0);
+   //cin.tie(0);
+   //cout.tie(0);
+    Maha maha;
+    return 0;
+}
