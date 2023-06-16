93423be2e3b9e64012d50c546b85788081784788
Md. Mahadi Hasan <mahadi.hasan.khan16@gmail.com>
2023-06-16 14:38:41 +0600

    Create CF 1832C.cpp

---
+++ Codeforces/CF 1832C.cpp
@@ -0,0 +1,57 @@
+/*
+MD. MAHADI HASAN
+Dept. of Computer Science and Telecommunication Engineering (CSTE).
+Noakhali Science and Technology University (NSTU).
+*/
+#include<bits/stdc++.h>
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define pi acos(-1)
+#define lcm(a,b) (a*b)/__gcd(a,b)
+#define all(v) v.begin(),v.end()
+#define w(x) long long x;scanf("%lld",&x);while(x--)
+#define ios ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define nl "\n"
+#define Case(cs) printf("Case %d: ",++cs)
+#define debug(x) cout<<x<<" ";
+typedef long long ll;
+using namespace std;
+ll mod=10e9+7;
+class Maha{
+public:
+    Maha()
+    {
+        w(t)
+        {
+            int n,i,ans=1,temp,k,a;
+            cin>>n;
+            cin>>a;
+            temp=a;
+            string track="none",state;
+            for(i=1;i<n;i++)
+            {
+                cin>>a;
+                if(a>temp)
+                    state="increasing";
+                else if(a<temp)
+                    state="decreasing";
+                else
+                    continue;
+                if(track!=state)
+                    ans++;
+                track=state,temp=a;
+            }
+            cout<<ans<<nl;
+        }
+    }
+};
+ 
+signed main()
+{
+    //ios::sync_with_stdio(0);
+   //cin.tie(0);
+   //cout.tie(0);
+    Maha maha;
+    return 0;
+}
