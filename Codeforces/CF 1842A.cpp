acad5b07a7ac055606b25eb327b49c533790e1b9
Md. Mahadi Hasan <mahadi.hasan.khan16@gmail.com>
2023-06-25 10:32:43 +0600

    Create CF 1842A.cpp

---
+++ Codeforces/CF 1842A.cpp
@@ -0,0 +1,57 @@
+/*
+MD. MAHADI HASAN
+Dept. of Computer Science and Telecommunication Engineering (CSTE).
+Noakhali Science and Technology University (NSTU).
+*/
+#include<bits/stdc++.h>
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define pi acos(-1)
+#define lcm(a,b) (a*b)/__gcd(a,b)
+#define all(v) v.begin(),v.end()
+#define w(x) long long x;scanf("%lld",&x);while(x--)
+#define ios ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define nl "\n"
+#define Case(cs) printf("Case %d: ",++cs)
+#define debug(x) cout<<x<<" ";
+typedef long long ll;
+using namespace std;
+ll mod=10e9+7;
+class Maha{
+public:
+    Maha()
+    {
+        w(t)
+        {
+            int n,m,i;
+            ll tsondu=0,tenzing=0,x;
+            cin>>n>>m;
+            for(i=0;i<n;i++)
+            {
+                cin>>x;
+                tsondu+=x;
+            }
+            for(i=0;i<m;i++)
+            {
+                cin>>x;
+                tenzing+=x;
+            }
+            if(tsondu>tenzing)
+                cout<<"Tsondu\n";
+            else if(tenzing>tsondu)
+                cout<<"Tenzing\n";
+            else
+                cout<<"Draw\n";
+        }
+    }
+};
+
+signed main()
+{
+    //ios::sync_with_stdio(0);
+   //cin.tie(0);
+   //cout.tie(0);
+    Maha maha;
+    return 0;
+}
