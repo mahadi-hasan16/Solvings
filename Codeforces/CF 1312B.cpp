0695ee02656c95e263c5c853f70c520fa5f4c718
Md. Mahadi Hasan <mahadi.hasan.khan16@gmail.com>
2023-06-29 00:51:55 +0600

    Create CF 1312B.cpp

---
+++ Codeforces/CF 1312B.cpp
@@ -0,0 +1,60 @@
+/*
+MD. MAHADI HASAN
+Dept. of Computer Science and Telecommunication Engineering (CSTE).
+Noakhali Science and Technology University (NSTU).
+*/
+#include<bits/stdc++.h>
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define pi acos(-1)
+#define lcm(a,b) (a*b)/__gcd(a,b)
+#define all(v) v.begin(),v.end()
+#define w(x) long long x;scanf("%lld",&x);while(x--)
+#define ios ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define nl "\n"
+#define Case(cs) printf("Case %d: ",++cs)
+#define debug(x) cout<<x<<" ";
+typedef long long ll;
+using namespace std;
+ll i,mod=10e9+7;
+
+struct Lamp
+{
+    int a,b;
+};
+
+class Maha{
+    static bool cmp(int a,int b)
+    {
+        if(a>b)
+            return true;
+        else
+            return false;
+    }
+public:
+    Maha()
+    {
+        w(t)
+        {
+            int n;
+            cin>>n;
+            int arr[n];
+            for(i=0;i<n;i++)
+                cin>>arr[i];
+            sort(arr,arr+n,cmp);
+            for(i=0;i<n;i++)
+                cout<<arr[i]<<" ";
+            cout<<nl;
+        }
+    }
+};
+
+signed main()
+{
+    //ios::sync_with_stdio(0);
+   //cin.tie(0);
+   //cout.tie(0);
+    Maha maha;
+    return 0;
+}
